0ce70b4aaef5688cd8d81fce0454bc9a91a3be5b
Attapon Peungsook <aon1840@Attapons-MBP-2.ST-244F>
2021-02-23 00:02:05 +0700

    sort_algorithm

---
+++ week6/sort_data.c
@@ -0,0 +1,35 @@
+#include<stdio.h> 
+
+int main() { 
+    int a[100], b, i, j, n; 
+    for ( i = 1;; i++)
+    {
+        printf("input a[%d] = ", i);
+        scanf("%d", &a[i]); 
+        if(a[i] < 0) break; 
+    }
+    n = i - 1; 
+    for ( i = 1; i < n; i++)
+    {
+        for (j = i+ 1; j <= n; j++)
+        {
+            if (a[i] < a[j]) continue;
+            else { 
+                b = a[i]; 
+                a[i] = a[j]; 
+                a[j] = b;
+            }
+            
+        }
+        
+    }
+
+    for ( i = 1; i <= n; i++)
+    {
+        printf("a[%d] = %d\n", i, a[i]); 
+        
+    }
+    
+    
+    
+}
--- week6/sum_column.c
+++ week6/sum_column.c
@@ -1,21 +1,65 @@
 #include<stdio.h> 
 int main() { 
     // รับมิติของแต่ละเมทริก 
-    int i, j, a[10][10], c[10], m, n; 
+    int i, j, matrix[10][10], sum_col[10], rows, column; 
     printf("matrix :");
-    scanf("%d, %d", &m, &n); 
+    scanf("%d, %d", &rows, &column); 
 
     // input element
     printf("input element of matrix A\n"); 
-    for (int i = 1; i < m; i++)
+    for (int i = 1; i <= rows; i++)
     {
-        for (int j = 1; j < n;j++)
+        for (int j = 1; j <= column;j++)
         {
-            printf("a[%d][%d] = ", i, j);
-            scanf("%d", &a[i][j]);
+            printf("matrix[%d][%d] = ", i, j);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    // เก็บค่า sum_column 
+    for ( i = 1; i <= column; i++)
+    {
+        sum_col[i] = 0;
+        for ( j = 1; j <= rows; j++)
+        {
+            sum_col[i] += matrix[i][j];
         }
         
     }
+    
+
+    // พิมพ์ผลข้อมูลเมทริก 
+    for ( i = 1; i <= rows; i++)
+    {
+        printf("\n");
+        for (j = 1; j <= column; j++)
+        {
+            printf("%6d", matrix[j][i]); // ต้องเป็น matrix [j][i]
+        }
+        
+    }
+    printf("\n");
+
+    // print ผลบวกของ column 
+    for (i = 1; i <= 8*column; i++)
+    {
+        printf("=");
+    }
+    printf("\n");
+
+    for ( i = 1; i <= column; i++)
+    {
+        printf("%6d", sum_col[i]);
+    }
+    printf("\n");
+
+    for (i = 1; i <= 8*column; i++)
+    {
+        printf("=");
+    }
+   
+    
+    
+    
     
 
 }
