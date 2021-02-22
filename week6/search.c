dd7726ec3cd5caca7e25c191c9e2d3dfcebbaf1a
Attapon Peungsook <aon1840@Attapons-MBP-2.ST-244F>
2021-02-23 00:28:28 +0700

    search word algorithm

---
+++ week6/search.c
@@ -0,0 +1,30 @@
+#include<stdio.h> 
+int main() { 
+    char word[40]; 
+    int i, n=0; 
+    printf("input sentence :"); 
+    gets(word);
+    
+    for ( i = 0; word[i]!='0'; i++)
+    {
+        if (word[i] == 'T' || word[i] == 't')
+        {
+            if (word[i+1]=='h')
+            {
+                if (word[i+2]=='e')
+                {
+                    if (word[i+3]==' ')
+                    {
+                        n++;
+                    }
+                    
+                }
+            }
+            
+        }
+        
+    }
+
+    printf("Number of word 'The = %d\n", n);
+    
+}
--- week6/sort_data.c
+++ week6/sort_data.c
@@ -2,13 +2,14 @@
 
 int main() { 
     int a[100], b, i, j, n; 
+    // input data ไปเรื่อยๆ 
     for ( i = 1;; i++)
     {
         printf("input a[%d] = ", i);
         scanf("%d", &a[i]); 
         if(a[i] < 0) break; 
     }
-    n = i - 1; 
+    n = i - 1; // ลบตำแหน่งสุดท้ายไป
     for ( i = 1; i < n; i++)
     {
         for (j = i+ 1; j <= n; j++)
