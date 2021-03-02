b85b1850be4970dbc8acfa1a29bc9fc73014ba94
Attapon Peungsook <aon1840@Attapons-MacBook-Pro-2.local>
2021-03-02 09:43:27 +0700

    learn_week5

---
+++ week5/read_number.c
@@ -0,0 +1,15 @@
+#include<stdio.h> 
+int main() { 
+    char call[10]; 
+    char read[10][10] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}; 
+    printf("Input phone number : "); 
+    gets(call); 
+    printf("read-number : ");
+    for (int i = 0; call[i] != '\0'; i++)
+    {
+
+
+    }
+    
+
+}
